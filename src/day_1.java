4d5d5918b90a75f2db669c83d9aab049596c5b31
mkemalsimsek <62071572+mkemalsimsek@users.noreply.github.com>
2020-05-12 17:23:48 +0100

    Update day_1.java

--- src/day_1.java
+++ src/day_1.java
@@ -1,5 +1,7 @@
 public class day_1 {
     public static void main(String[] args) {
         System.out.println("User is able to create channels");
+        
+        // Brand new changes from github
     }
 }
