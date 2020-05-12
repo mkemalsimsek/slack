96b09b82ac72b8cb0bd25c4c0bba3b9aecc18e9a
mkemalsimsek <mkemalsimsek@hotmail.com>
2020-05-12 17:25:32 +0100

    day_1 Package created

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -1,4 +1,6 @@
-public class day_1 {
+package day_1;
+
+public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create channels");
     }
--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -1,5 +1,8 @@
+package day_1;
+
 public class Login {
     public static void main(String[] args) {
         System.out.println("Thia is login class");
     }
 }
+
