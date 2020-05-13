cde1badf4f03637637de9e6551c7eaf3c63b4103
mkemalsimsek <mkemalsimsek@hotmail.com>
2020-05-13 15:35:12 +0100

    update Login.Java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -3,6 +3,11 @@ package day_1;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Thia is login class");
+
+
+
+
+        // This iline is written from Intellij
     }
 }
 
