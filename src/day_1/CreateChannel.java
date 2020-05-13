4c0c4270fe037abcf76f51ecd29e22cc227aab3a
mkemalsimsek <mkemalsimsek@hotmail.com>
2020-05-13 15:58:03 +0100

    update CreateChannel.Java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,12 @@ public class CreateChannel {
         System.out.println("User is able to create channels");
         
         // Brand new changes from github.....
+
+        int x=0; //changed from intellij
+        if (x==0){
+            System.out.println("equal");
+        }
+
+
     }
 }
